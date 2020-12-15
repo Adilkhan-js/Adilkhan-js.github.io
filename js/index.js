386734083c0382aba099e8b7d64e114d952bc63b
Adilkhan Almaz <adilkhanalmaz@MacBook-Air-Adilkhan.local>
2020-12-16 03:12:12 +0600

    My second commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        // autoplay:true,
+        // autoplaySpeed:500,
+        waitForAimate:false,
+        centerMode:true,
+        
+    });
+});
